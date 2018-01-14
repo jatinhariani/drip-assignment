86f4e3d17fdb7a77ab90d9f6d4fb93287b381266
Jatin Hariani <jtnh321@gmail.com>
2018-01-14 11:11:13 +0530

    some more steps

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="signup">
-    <el-row type="flex" justify="center"">
+    <el-row type="flex" justify="center">
       <el-col :span="18">
         <el-steps :active="currentStep" align-center finish-status="success">
           <el-step title="Step 1" description="Some description"></el-step>
@@ -14,6 +14,8 @@
       <el-col :span="12">
         <step-0 @success="next" v-if="currentStep == 0"></step-0>
         <step-1 @success="next" v-if="currentStep == 1"></step-1>
+        <step-2 @success="next" v-if="currentStep == 2"></step-2>
+        <step-3 @success="next" v-if="currentStep == 3"></step-3>
       </el-col>
     </el-row>
   </div>
@@ -21,6 +23,8 @@
 <script>
 import Step0 from './form-steps/Step0'
 import Step1 from './form-steps/Step1'
+import Step2 from './form-steps/Step2'
+import Step3 from './form-steps/Step3'
 
 export default {
   data () {
@@ -30,7 +34,9 @@ export default {
   },
   components: {
     Step0,
-    Step1
+    Step1,
+    Step2,
+    Step3
   },
   methods: {
     next () {
@@ -42,8 +48,8 @@ export default {
 </script>
 <style lang="scss" scoped>
 .form-row {
-  padding-bottom: 50px;
-  padding-top: 50px;
+  padding-bottom: 30px;
+  padding-top: 30px;
 }
 .lead {
   font-size: 24px;
--- src/components/form-steps/Step1.vue
+++ src/components/form-steps/Step1.vue
@@ -1,12 +1,58 @@
 <template>
   <div>
-    step 1
-    <a href="#" @click.prevent="$emit('success')">Next</a>
+    <el-form ref="details-form" :model="sizeForm" label-width="120px" size="mini" :rules="rules">
+      <el-form-item label="Name" prop="name">
+        <el-input v-model="sizeForm.name" placeholder="John Doe"></el-input>
+      </el-form-item>
+      <el-form-item label="Email" prop="email">
+        <el-input v-model="sizeForm.email" placeholder="email@domain.com"></el-input>
+      </el-form-item>
+      <el-form-item label="Phone" prop="phone">
+        <el-input v-model="sizeForm.phone" placeholder="9999999999"></el-input>
+      </el-form-item>
+      <el-form-item label="PAN No" prop="pan">
+        <el-input v-model="sizeForm.pan" placeholder="AAAAPA1111A"></el-input>
+      </el-form-item>
+      <el-form-item label="Company Name">
+        <el-input v-model="sizeForm.company" placeholder="Acme Industries"></el-input>
+      </el-form-item>
+      <el-form-item size="large">
+        <el-button type="primary" @click="onSubmit">Next</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
-  name: 'dude'
+  data () {
+    return {
+      sizeForm: {
+        name: '',
+        email: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: 'Please input your Name', trigger: 'blur' },
+          { min: 3, message: 'Name should be at least 3 characters in length', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, type: 'email', message: 'Please input correct email address', trigger: 'blur,change' }
+        ],
+        pan: [
+          { required: true, pattern: /(^([A-Z]{3}P[A-Z])([0-9]{4})([A-Z]{1})$)/, message: 'Please input correct PAN Card Number', trigger: 'blur' }
+        ],
+        phone: [
+          { pattern: /(^([0-9]{10})$)/, message: 'Please input a valid phone number', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$emit('success')
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
